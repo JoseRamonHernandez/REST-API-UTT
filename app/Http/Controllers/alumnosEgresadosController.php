609a80c4b5fbea591eadf3e03a9cfefe6fa7ee86
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2023-02-01 23:00:21 -0600

    nueva ruta al llamado 'getAlumnosEgresados'

--- app/Http/Controllers/alumnosEgresadosController.php
+++ app/Http/Controllers/alumnosEgresadosController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Models\alumnos_egresados;
+use App\Models\alumnos_egresados;
 
 class alumnosEgresadosController extends Controller
 {
