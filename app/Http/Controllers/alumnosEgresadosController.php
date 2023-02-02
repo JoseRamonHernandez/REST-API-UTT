4e2dda8ef7a3e6fda0014338dd0c02ff0e8f47e4
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2023-02-01 23:06:15 -0600

    nuevo controlador para buscar por id

--- app/Http/Controllers/alumnosEgresadosController.php
+++ app/Http/Controllers/alumnosEgresadosController.php
@@ -10,4 +10,14 @@ class alumnosEgresadosController extends Controller
     public function getAlumnosEgresados(){
         return response()->json(alumnos_egresados::all(),200);
     }
+
+    public function getAlumnoEgresado($id){
+        $alumno = alumnos_egresados::find($id);
+        if(empty($alumno)){
+            return response()->json(['Mensaje'=>'Registro no encontrado'],404);
+        }
+        else{
+        return response()->json($alumno,200);
+        }
+    }
 }
