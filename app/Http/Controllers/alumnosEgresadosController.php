185a07cd7c4f62de0641fcebf9b729a7522bee75
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2023-02-01 22:42:03 -0600

    funcion de regresar a todos los alumnos egresados en la base de datos

--- app/Http/Controllers/alumnosEgresadosController.php
+++ app/Http/Controllers/alumnosEgresadosController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\alumnos_egresados;
 
 class alumnosEgresadosController extends Controller
 {
-    //
+    public function getAlumnosEgresados(){
+        return response()->json(alumnos_egresados::all(),200);
+    }
 }
